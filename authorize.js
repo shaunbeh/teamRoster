b4ec5064fb0b5f1fb9a3823a68a47992c7076d62
shaunbeh <shahin.behmand@gmail.com>
2023-07-07 22:03:02 +0330

    manual authorization

---
+++ authorize.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
+
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+const TOKEN_PATH = "token.json";
+
+require("dotenv").config();
+
+authorize(process.env, listMajors);
+
+function authorize(credentials, callback) {
+  const { CLIENT_SECRET, CLIENT_ID, REDIRECT_URIS } = credentials;
+  const oAuth2Client = new google.auth.OAuth2({
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    redirectUri: REDIRECT_URIS,
+  });
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+  console.log("Authorize this app by visiting this url:", authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question("Enter the code from that page here: ", (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error("Error while trying to retrieve access token", err);
+      oAuth2Client.setCredentials(token);
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log("Token stored to", TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+function listMajors(auth) {
+  const sheets = google.sheets({ version: "v4", auth });
+  sheets.spreadsheets.values.get(
+    {
+      spreadsheetId: "1TPyB_HGEHICUbEMVxweR_mBgNJw1gYOc3ekGT2wr-8I",
+      range: "Class Data!A2:E",
+    },
+    (err, res) => {
+      if (err) return console.log("The API returned an error: " + err);
+      const rows = res.data.values;
+      if (rows.length) {
+        console.log("Name, Major:");
+        rows.map((row) => {
+          console.log(`${row[0]}, ${row[4]}`);
+        });
+      } else {
+        console.log("No data found.");
+      }
+    }
+  );
+}
